eb4fd9d5da533594ffbaee8ffd8a670a249a944c
Eugene <eugeneyshthom@gmail.com>
2025-05-13 12:21:42 -0400

    Initial Astro Blog Template

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://euhystho.github.io',
+    integrations: [mdx(), sitemap(), svelte()],
+});
