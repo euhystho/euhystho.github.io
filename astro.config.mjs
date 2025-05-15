82c07f7bcb43413beb01dac4a7b0937485499eaa
Eugene Thompson <eugeneyshthom@gmail.com>
2025-05-15 11:15:19 -0400

    Removed Analytics and Restructured

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwindcss from '@tailwindcss/vite';
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eugenehysthompson.com",
   output: "static",
-  integrations: [mdx(), sitemap(), svelte(), partytown()],
+  integrations: [mdx(), sitemap(), svelte()],
 
   vite: {
     plugins: [tailwindcss()],
