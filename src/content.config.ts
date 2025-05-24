c9014aa3966b70254dfd2f709b763e6f70f87e97
Eugene Thompson <eugeneyshthom@gmail.com>
2025-05-24 12:36:14 -0400

    closes #2: Fixed Content Collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,14 @@
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const projectsCollection = defineCollection({
   // Load MDX files in the `src/content/projects/` directory.
-  loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
+  loader: glob({ base: "./src/projects", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    image: z.string(),
+    image: z.string().optional(),
     tags: z.array(z.string()),
     github: z.string().url().optional(),
     demo: z.string().url().optional(),
@@ -19,7 +19,7 @@ const projectsCollection = defineCollection({
 
 const writingCollection = defineCollection({
   // Load MDX files in the `src/content/projects/` directory.
-  loader: glob({ base: "./src/content/writing", pattern: "**/*.{md,mdx}" }),
+  loader: glob({ base: "./src/writings", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -27,7 +27,7 @@ const writingCollection = defineCollection({
     tags: z.array(z.string()),
     featured: z.boolean().default(false),
     date: z.coerce.date(),
-    mod_date: z.coerce.date()
+    modDate: z.coerce.date()
   }),
 });
 
