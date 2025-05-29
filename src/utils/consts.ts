5129e1914d1d15aedfa27551ddb2a1edd529bafe
Eugene Thompson <eugeneyshthom@gmail.com>
2025-05-29 16:24:57 -0400

    fix #3: Added 404, Header & Footer

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Eugene Thompson";
+// Head Properties
+export const SITE_AUTHOR = "Eugene Thompson";
 export const SITE_DESCRIPTION = "Eugene Thompson's personal website showcasing projects, academic achievements, and professional aspirations";
+export const KEYWORDS = "";
+
+// Theme Properties
+export const ACCENT_COLOR = "#FFC40C";
