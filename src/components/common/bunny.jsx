f0592c5cf8a30e3fcdcd00614ac473aba69cc511
Eugene Thompson <eugeneyshthom@gmail.com>
2025-05-30 01:44:05 -0400

    Fixed Animation and Pathing?

--- src/components/common/bunny.jsx
+++ src/components/common/bunny.jsx
@@ -1,11 +1,21 @@
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+import { Suspense, lazy } from 'react';
+
+const DotLottieReact = lazy(() => 
+  import('@lottiefiles/dotlottie-react').then(module => ({ 
+    default: module.DotLottieReact 
+  }))
+);
 
 export default function LottiePlayer() {
-    return (
-        <DotLottieReact
-        src="/bunny.lottie"
+  return (
+    <Suspense fallback={<div>Loading animation...</div>}>
+      <DotLottieReact
+        src="bunny.lottie"
         loop
         speed={0.85}
-        autoplay />
-    );
-};
+        autoplay 
+      />
+    </Suspense>
+  );
+}
